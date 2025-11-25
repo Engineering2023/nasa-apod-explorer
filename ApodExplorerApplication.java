abfc37826b7ce26f6027ff2614d06395acb9779a
Harshitha <119332695+Engineering2023@users.noreply.github.com>
2025-11-25 11:11:11 +0530

    ApodExplorerApplication.java

---
+++ ApodExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.example.apod;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ApodExplorerApplication {
+public static void main(String[] args) {
+SpringApplication.run(ApodExplorerApplication.class, args);
+}
+}
